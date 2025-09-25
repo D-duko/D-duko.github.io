feeaf9d99e133a049a71645b68a5c257868cc398
D-duko <jan091@gmx.at>
2025-09-25 19:41:41 +0200

    ADDED CSS + JS

---
+++ script.js
@@ -0,0 +1,4 @@
+function infopage130094() {
+    alert("You will be redirected to a 3rd-party website. Click Reload to abort.")
+    window.location.replace("https://www.cyberark.com/what-is/active-directory/")
+}
